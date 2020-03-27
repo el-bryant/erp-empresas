4b3f821812c349ccf87d1ee65c17495b3babe13a
el-bryant <bry.y.valen@gmail.com>
2020-03-26 22:11:31 -0500

    Detalles de Estado Financiero

--- estado_financiero.php
+++ estado_financiero.php
@@ -11,36 +11,48 @@ include_once('layouts/header.php');
 
                 <!--Título de sección-->
                 <div class="card rounded-pill border-0 bg-white sombra mt-3 mb-5">
-                    <div class="row d-flex justify-content-between letra-azul">
-                        <p class="bold my-1 ml-5">Estado financiero</p>
-                        <p class="my-1 mr-5 pr-5">20 - 12 - 2019</p>
+                    <div class="row justify-content-between letra-azul">
+                        <div class="d-flex flex-row ml-5">
+                            <i class="fas fa-file-invoice-dollar fa-md my-auto mr-3"></i>
+                            <p class="bold my-1">Estado Financiero</p>
+                        </div>
+                        <p class="my-1 mr-5">20 - 12 - 2019</p>
                     </div>
                 </div>
                 <!--Título de sección-->
 
                 <!--Contenedores superiores-->
                 <div class="row d-flex justify-content-between px-4">
-                    <div class="col-3">
-                        <div class="card card-dashboard border-0 sombra">
-                            <h5 class="card-header bg-caja-gerencia text-white px-2">Caja Gerencia</h5>
+                    <div class="col-4">
+                        <div class="card card-dashboard border-0 sombra mx-3">
+                            <div class="card-header bg-caja-gerencia text-white d-flex flex-row align-items-center justify-content-between px-3">
+                                <h5 class="bold my-0">Caja Gerencia</h5>
+                                <i class="fas fa-building fa-lg"></i>
+                            </div>
                             <div class="card-body">
-                                <div class="card-title bold h4 text-center text-gris">S/ 700.00</div>
+                                <div class="card-title bold h3 text-center text-gris">S/ 700.00</div>
                             </div>
                         </div>
                     </div>
-                    <div class="col-3">
-                        <div class="card card-dashboard border-0 sombra">
-                            <h5 class="card-header bg-caja-bancaria text-white px-2">Cuenta Bancaria</h5>
+                    <div class="col-4">
+                        <div class="card card-dashboard border-0 sombra mx-3">
+                            <div class="card-header bg-caja-bancaria text-white d-flex flex-row align-items-center justify-content-between px-3">
+                                <h5 class="bold my-0">Cuenta Bancaria</h5>
+                                <i class="fas fa-credit-card fa-lg"></i>
+                            </div>
                             <div class="card-body">
-                                <div class="card-title bold h4 text-center text-gris">S/ 700.00</div>
+                                <div class="card-title bold h3 text-center text-gris">S/ 700.00</div>
                             </div>
                         </div>
                     </div>
-                    <div class="col-3">
-                        <div class="card card-dashboard border-0 sombra">
-                            <h5 class="card-header bg-total text-white px-2">Total</h5>
+                    <div class="col-4">
+                        <div class="card card-dashboard border-0 sombra mx-3">
+                            <div class="card-header bg-total text-white d-flex flex-row align-items-center justify-content-between px-3">
+                                <h5 class="bold my-0">Total</h5>
+                                <i class="fas fa-database fa-lg"></i>
+                            </div>
                             <div class="card-body">
-                                <div class="card-title bold h4 text-center text-gris">S/ 700.00</div>
+                                <div class="card-title bold h3 text-center text-gris">S/ 700.00</div>
                             </div>
                         </div>
                     </div>
@@ -52,24 +64,24 @@ include_once('layouts/header.php');
                     <div class="col-6">
                         <div class="card card-dashboard border-0 sombra">
                             <div class="row px-4 py-2 justify-content-center">
-                                <div class="col-3 badge badge-oscuro mt-2 mr-3 py-2 pl-3 text-left">Saldo cajas:</div>
-                                <div class="col-8 badge badge-claro mt-2 py-2 pl-3 text-left">S/ 700.00</div>
-                                <div class="col-3 badge badge-oscuro my-2 mr-3 py-2 pl-3 text-left">Ingresos:</div>
-                                <div class="col-8 badge badge-claro my-2 py-2 pl-3 text-left">S/ 700.00</div>
-                                <div class="col-3 badge badge-oscuro mb-2 mr-3 py-2 pl-3 text-left">Egresos:</div>
-                                <div class="col-8 badge badge-claro mb-2 py-2 pl-3 text-left">S/ 700.00</div>
+                                <div class="text-left t-13 col-3 badge badge-oscuro mr-3 mt-2 pl-3 py-2">Saldo cajas:</div>
+                                <div class="text-left t-13 col-8 badge badge-claro mt-2 pl-3 py-2">S/ 700.00</div>
+                                <div class="text-left t-13 col-3 badge badge-oscuro mr-3 my-2 pl-3 py-2">Ingresos:</div>
+                                <div class="text-left t-13 col-8 badge badge-claro my-2 pl-3 py-2">S/ 700.00</div>
+                                <div class="text-left t-13 col-3 badge badge-oscuro mr-3 mb-2 pl-3 py-2">Egresos:</div>
+                                <div class="text-left t-13 col-8 badge badge-claro mb-2 pl-3 py-2">S/ 700.00</div>
                             </div>
                         </div>
                     </div>
                     <div class="col-6">
                         <div class="card card-dashboard border-0 sombra">
                             <div class="row px-4 py-2 justify-content-center">
-                                <div class="col-3 badge badge-oscuro mt-2 mr-3 py-2 pl-3 text-left">Saldo cajas:</div>
-                                <div class="col-8 badge badge-claro mt-2 py-2 pl-3 text-left">S/ 700.00</div>
-                                <div class="col-3 badge badge-oscuro my-2 mr-3 py-2 pl-3 text-left">Ingresos:</div>
-                                <div class="col-8 badge badge-claro my-2 py-2 pl-3 text-left">S/ 700.00</div>
-                                <div class="col-3 badge badge-oscuro mb-2 mr-3 py-2 pl-3 text-left">Egresos:</div>
-                                <div class="col-8 badge badge-claro mb-2 py-2 pl-3 text-left">S/ 700.00</div>
+                                <div class="text-left t-13 col-3 badge badge-oscuro mr-3 mt-2 pl-3 py-2">Saldo cajas:</div>
+                                <div class="text-left t-13 col-8 badge badge-claro mt-2 pl-3 py-2">S/ 700.00</div>
+                                <div class="text-left t-13 col-3 badge badge-oscuro mr-3 my-2 pl-3 py-2">Ingresos:</div>
+                                <div class="text-left t-13 col-8 badge badge-claro my-2 pl-3 py-2">S/ 700.00</div>
+                                <div class="text-left t-13 col-3 badge badge-oscuro mr-3 mb-2 pl-3 py-2">Egresos:</div>
+                                <div class="text-left t-13 col-8 badge badge-claro mb-2 pl-3 py-2">S/ 700.00</div>
                             </div>
                         </div>
                     </div>
--- layouts/admin_menu.php
+++ layouts/admin_menu.php
@@ -1,10 +1,15 @@
 <ul class="list-unstyled">
+    
+    <!--Estado financiero-->
     <li>
         <a class="nav-link d-flex flex-row align-items-center pl-0" id="navEstadoFinanciero" href="estado_financiero.php" role="tab">
             <i class="fas fa-file-invoice-dollar fa-md col-1 px-auto"></i>
             <p class="col-11 my-0 pl-2">Estado financiero</p>
         </a>
     </li>
+    <!--Estado financiero-->
+
+    <!--Ingresos-->
     <li>
         <div class="nav-link d-flex flex-row align-items-center">
             <i class="fas fa-plus-circle fa-md col-1 pl-0"></i>
@@ -13,20 +18,30 @@
             </a>
         </div>
         <ul class="collapse list-unstyled" id="ingresosSM">
+
+            <!--Venta-->
             <li>
                 <a class="nav-link d-flex flex-row align-items-center ml-4 pl-0" id="subnavVenta" href="venta.php" role="tab">
                     <i class="fas fa-shopping-cart fa-md col-1 px-auto"></i>
                     <p class="col-11 my-0 ml-0 pl-2">Venta</p>
                 </a>
             </li>
+            <!--Venta-->
+
+            <!--Otros ingresos-->
             <li>
                 <a class="nav-link d-flex flex-row align-items-center ml-4 pl-0" id="subnavOtrosIngresos" href="otros_ingresos.php" role="tab">
                     <i class="fas fa-money-check-alt fa-md col-1 px-auto"></i>
                     <p class="col-11 my-0 ml-0 pl-2">Otros ingresos</p>
                 </a>
             </li>
+            <!--Otros ingresos-->
+
         </ul>
     </li>
+    <!--Ingresos-->
+
+    <!--Egresos-->
     <li>
         <div class="d-flex flex-row align-items-center nav-link">
             <i class="fas fa-minus-circle fa-md col-1 pl-0"></i>
@@ -35,38 +50,57 @@
             </a>
         </div>
         <ul class="collapse list-unstyled" id="egresosSM">
+
+            <!--Inversión-->
             <li>
                 <a class="nav-link d-flex flex-row align-items-center ml-4 pl-0" id="subnavInversion" href="inversion.php" role="tab">
                     <i class="fas fa-coins fa-md col-1 px-auto"></i>
                     <p class="col-11 my-0 ml-0 pl-2">Inversión</p>
                 </a>
             </li>
+            <!--Inversión-->
+            
+            <!--Otros egresos-->
             <li>
                 <a class="nav-link d-flex flex-row align-items-center ml-4 pl-0" id="subnavOtrosEgresos" href="otros_egresos.php" role="tab">
                     <i class="fas fa-money-bill fa-md col-1 px-auto"></i>
                     <p class="col-11 my-0 ml-0 pl-2">Otros egresos</p>
                 </a>
             </li>
+            <!--Otros egresos-->
+
         </ul>
     </li>
+    <!--Egresos-->
+
+    <!--Conceptos-->
     <li>
         <a class="nav-link d-flex flex-row align-items-center pl-0" id="navConceptos" href="conceptos.php" role="tab">
             <i class="fas fa-list-alt fa-md col-1 px-auto"></i>
             <p class="col-11 my-0 pl-2">Conceptos</p>
         </a>
     </li>
+    <!--Conceptos-->
+
+    <!--Colaboradores-->
     <li>
         <a class="nav-link d-flex flex-row align-items-center pl-0" id="navColaboradores" href="colaboradores.php" role="tab">
             <i class="fas fa-user-tie fa-md col-1 px-auto"></i>
             <p class="col-11 my-0 pl-2">Colaboradores</p>
         </a>
     </li>
+    <!--Colaboradores-->
+
+    <!--Informes-->
     <li>
         <a class="nav-link d-flex flex-row align-items-center pl-0" id="navInformes" href="informes.php" role="tab">
             <i class="fas fa-file-contract fa-md col-1 px-auto"></i>
             <p class="col-11 my-0 pl-2">Informes</p>
         </a>
     </li>
+    <!--Informes-->
+
+    <!--Tareas a realizar-->
     <li>
         <div class="d-flex flex-row align-items-center nav-link">
             <i class="fas fa-clipboard-list fa-md col-1 pl-0"></i>
@@ -75,14 +109,21 @@
             </a>
         </div>
         <ul class="collapse list-unstyled" id="tareasSM">
+
+            <!--Cuentas por cobrar-->
             <li>
                 <a class="nav-link d-flex flex-row align-items-center ml-4 pl-0" id="subnavCuentas" href="cuentas_por_cobrar.php" role="tab">
                     <i class="fas fa-search-dollar fa-md col-1 px-auto"></i>
                     <p class="col-11 my-0 pl-2">Cuentas por cobrar</p>
                 </a>
             </li>
+            <!--Cuentas por cobrar-->
+
         </ul>
     </li>
+    <!--Tareas a realizar-->
+
+    <!--Contactos-->
     <li>
         <div class="d-flex flex-row align-items-center nav-link">
             <i class="fas fa-user-friends fa-md col-1 pl-0"></i>
@@ -91,50 +132,75 @@
             </a>
         </div>
         <ul class="collapse list-unstyled" id="contactosSM">
+
+            <!--Usuarios-->
             <li>
                 <a class="nav-link d-flex flex-row align-items-center ml-4 pl-0" id="subnavUsuarios" href="usuarios.php" role="tab">
                     <i class="fas fa-user fa-md col-1 px-auto"></i>
                     <p class="col-11 my-0 pl-2">Usuarios</p>
                 </a>
             </li>
+            <!--Usuarios-->
+
+            <!--Clientes-->
             <li>
                 <a class="nav-link d-flex flex-row align-items-center ml-4 pl-0" id="subnavClientes" href="clientes.php" role="tab">
                     <i class="fas fa-user-check fa-md col-1 px-auto"></i>
                     <p class="col-11 my-0 pl-2">Clientes</p>
                 </a>
             </li>
+            <!--Clientes-->
+
+            <!--Prospectos-->
             <li>
                 <a class="nav-link d-flex flex-row align-items-center ml-4 pl-0" id="subnavProspectos" href="prospectos.php" role="tab">
                     <i class="fas fa-user-plus fa-md col-1 px-auto"></i>
                     <p class="col-11 my-0 pl-2">Prospectos</p>
                 </a>
             </li>
+            <!--Prospectos-->
+
+            <!--Proveedores-->
             <li>
                 <a class="nav-link d-flex flex-row align-items-center ml-4 pl-0" id="subnavProveedores" href="proveedores.php" role="tab">
                     <i class="fas fa-user-tag fa-md col-1 px-auto"></i>
                     <p class="col-11 my-0 pl-2">Proveedores</p>
                 </a>
             </li>
+            <!--Proveedores-->
+
         </ul>
     </li>
+    <!--Contactos-->
+
+    <!--Notificaciones-->
     <li>
         <a class="nav-link d-flex flex-row align-items-center pl-0" id="navNotificaciones" href="notificaciones.php" role="tab">
             <i class="fas fa-bell fa-md col-1 px-auto"></i>
             <p class="col-11 my-0 pl-2">Notificaciones</p>
         </a>
     </li>
+    <!--Notificaciones-->
+    
+    <!--Calendario-->
     <li>
         <a class="nav-link d-flex flex-row align-items-center pl-0" id="navCalendario" href="calendario.php" role="tab">
             <i class="fas fa-calendar-alt fa-md col-1 px-auto"></i>
             <p class="col-11 my-0 pl-2">Calendario</p>
         </a>
     </li>
+    <!--Calendario-->
+
+    <!--Configuración-->
     <li>
         <a class="nav-link d-flex flex-row align-items-center pl-0" id="navConfiguracion" href="configuracion.php" role="tab">
             <i class="fas fa-user-cog fa-md col-1 px-auto"></i>
             <p class="col-11 my-0 pl-2">Configuración</p>
         </a>
     </li>
+    <!--Configuración-->
+    
+    <!--Inventario-->
     <li>
         <div class="d-flex flex-row align-items-center nav-link">
             <i class="fas fa-barcode fa-md col-1 pl-0"></i>
@@ -143,24 +209,36 @@
             </a>
         </div>
         <ul class="collapse list-unstyled" id="inventarioSM">
+            
+            <!--Stock-->
             <li>
                 <a class="nav-link d-flex flex-row align-items-center ml-4 pl-0" id="subnavStock" href="stock.php" role="tab">
                     <i class="fas fa-dumpster fa-md col-1 px-auto"></i>
                     <p class="col-11 my-0 pl-2">Stock</p>
                 </a>
             </li>
+            <!--Stock-->
+
+            <!--Lote-->
             <li>
                 <a class="nav-link d-flex flex-row align-items-center ml-4 pl-0" id="subnavLote" href="lote.php" role="tab">
                     <i class="fas fa-box fa-md col-1 px-auto"></i>
                     <p class="col-11 my-0 pl-2">Lote</p>
                 </a>
             </li>
+            <!--Lote-->
+
+            <!--Kardex-->
             <li>
                 <a class="nav-link d-flex flex-row align-items-center ml-4 pl-0" id="subnavKardex" href="kardex.php" role="tab">
                     <i class="fas fa-grip-horizontal fa-md col-1 px-auto"></i>
                     <p class="col-11 my-0 p l-2">Kardex</p>
                 </a>
             </li>
+            <!--Kardex-->
+            
         </ul>
     </li>
+    <!--Inventario-->
+
 </ul>
